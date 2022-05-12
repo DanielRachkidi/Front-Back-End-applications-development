974656793dd3f81a8901cdad4663d9a3c5a7ce48
daniel <daniel@rachkidi.com>
2022-05-12 20:15:49 +0200

    first commit

--- Exercise/JS/main.js
+++ Exercise/JS/main.js
@@ -1,6 +1,6 @@
 
 /*
-const a1 = document.querySelector('div')
+const a1 = document.querySelector('.square')
 a1.addEventListener('click', function(){
     console.log('hello world')
     a1.style.display = 'none'
@@ -8,14 +8,26 @@ a1.addEventListener('click', function(){
 */
 
 
-
-        const square = document.querySelector("div");
+       const square = document.querySelector(".square");
         square.addEventListener("click",function(){
             console.log('hello world')
+
+            
+              if (document.querySelector('.square').style.visibility = 'hidden')
+              {
+              
+                setTimeout("document.querySelector('.square').style.visibility = 'visible'", 1000);
+             }
+            
+
+            
             const i = Math.floor(Math.random()*700)+1;
             const j = Math.floor(Math.random()*700)+1;
             square.style.left = i+"px";
             square.style.top = j+"px";
 
+            
+
         })
-        
+
+     
