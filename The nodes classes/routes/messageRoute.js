1f9164982ed4b0ef99a6941597f169781d4985de
daniel <daniel@rachkidi.com>
2022-06-08 17:52:06 +0200

    changes

--- The nodes classes/routes/messageRoute.js
+++ The nodes classes/routes/messageRoute.js
@@ -15,5 +15,37 @@ Router.post('/', async (req, res) => {
     await messageObj.save()
     return res.status(200).json(messageObj)
 })
+Router.put('/:index', (req, res) => {
+    const {index} = req.params // from the route
+    // const {test} = req.query // For params from url ?test=toto
+    
+    const {_v} = req.body // from the body
+    
+    if (typeof messages[index] != 'undefined') {
+        if (typeof _v != 'undefined') {
+            messages[index]._v = _v
+        }
+    } else {
+        return res.status(500).json({"msg": "message not found !"})
+    }
+
+    return res.status(200).json(messages[index])
+})
+
+
+Router.delete('/:_id', async (req, res) => {
+    const {id} = req.params
+
+
+    if (typeof messages[_id] != 'undefined') {
+        todos.splice(_id, 1)
+        return res.status(200).json({"msg": "Todo well deleted !"})
+    } else {
+        return res.status(500).json({"msg": "Todo not found !"})
+    }
+   
+    })
+   
+
 
 module.exports = Router
