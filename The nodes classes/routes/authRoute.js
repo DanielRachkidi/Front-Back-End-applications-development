a38b3fd282d4cde51f5e6b252a33c1bfc1edf6dd
daniel <daniel@rachkidi.com>
2022-06-13 21:09:44 +0200

    Changes

--- The nodes classes/routes/authRoute.js
+++ The nodes classes/routes/authRoute.js
@@ -35,15 +35,76 @@ Router.post('/register', async(req, res) => {
  * if it's good send back the user
  * try to add session to express
  */
+/*
+
 Router.post('/login', async (req, res) => {
-    const {email, password} = req.body
+    const {email, password} = req.params
 
     if ( (email && email !== "") && (password && password !== "")) {
-        const user = await userModel.findOne({'email': email})
+        const user = await userModel.findOne({'email': email, 'password':password})
         console.log(user)
+
+
+        
+        
     } else {
         res.status(500).json({"msg": "Email and password are required !"})
     }
-})
+   
+})*/
+
+
+
+Router.post('/login/:index', async (req, res) => {
+    const  email  =  req.body.email;
+    const  password  =  req.body.password;
+    const {index} = req.params
+
+    await userModel.findOne({_id: index})
+    
+        if ((password=="" || password !== "password") ||  (!email == email || email =="")){
+            const  result  =  bcrypt.compare(password == userModel.password);
+            if(!result) {return  res.status(401).send('Password not valid!');}
+
+            return  res.status(500).send('Server error!');}
+
+
+      
+
+        res.status(200)
+        res.json({
+          "answer":"Success",
+        })
+
+    });
+
+
+
+/*
+
+Router.post('/login', (req, res)=> {
+    var email = req.body.email;
+    var password = req.body.password
+   
+                if (data[0].password == password) {
+                    req.session['id']=data[0].ID;
+                    req.session.email = data.email;
+                    res.redirect('/');
+                }else {
+                    res.status(404).send('This password is not incorrect!').end();
+                }
+            
+        
+    
+});*/
+
+Router.get('/session/:index', async (req, res) => {
+    const {index} = req.params
+
+    await userModel.findOne({_id: index})
+    console.log(req.session && req.session.email);
+    res.end(`${req.session && req.session.email}`)
+});
+  
 
 module.exports = Router
